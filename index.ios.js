dcfa473cce7d64d05ff6ee9924a963d602eea57b
lixd <lixingda2012@foxmail.com>
2016-06-06 17:00:56 +0800

    统一规划主页的备注风格

--- index.ios.js
+++ index.ios.js
@@ -3,36 +3,38 @@ import React,{Component}from 'react';
 import{
   AppRegistry
 } from 'react-native';
-//网易
+//1、网易
 // var appForWangyi=require('./wangyi/main');
 // AppRegistry.registerComponent('Box',() => appForWangyi);
-//携程
+//2、携程
 // var appForCtrip=require('./ctrip/main');
 // AppRegistry.registerComponent('Box',() => appForCtrip);
-//列表页跳转
+//3、列表页跳转
 // var NV=require('./navigator/main');
 // AppRegistry.registerComponent('Box',() => NV);
-//TextInput
+//4、TextInput:输入框p113
 // var appForGaode=require('./TextInput/main');
 // AppRegistry.registerComponent('Box',() => appForGaode);
-//AutomaticallyPrompts
+//5、AutomaticallyPrompts:自动提示功能p114
 // var autoForApp=require('./AutomaticallyPrompts/main');
 // AppRegistry.registerComponent('Box',() => autoForApp);
-//Touchanle
+//6、Touchable:点击时背景阴影效果的背景颜色p121
 // var TouchableApp=require('./TouchableClass/main');
 // AppRegistry.registerComponent('Box',() => TouchableApp);
-// TouchableOpacityApp
+//7、TouchableOpacityApp:点击时不用设置背景色，也可呈现上面的效果p123
 // var TouchableOpacityApp=require('./TouchableOpacity/main');
 // AppRegistry.registerComponent('Box',() => TouchableOpacityApp);
-//Image组件
+//8、Image:设置图片链接p125
 // var ImageForApp=require('./ImageClass/main');
 // AppRegistry.registerComponent('Box',() => ImageForApp);
-// TabBarIos组件 类似于QQtab切换
+//9、TabBarIos:类似于QQtab切换p129
 // var TabBarForApp=require('./TabBarIosClass/main');
 // AppRegistry.registerComponent('Box',() => TabBarForApp);
-// WebView组件
+//10、WebView:加载外链网址或者本地的htmlp133
 // var WebViewForApp=require('./WebViewClass/main');
 // AppRegistry.registerComponent('Box',() => WebViewForApp);
-// appForweibo
+//11、appForweibo:加载微博OAuth授权登陆p137
 var appForweibo=require('./WeiBoOAuth/main');
+// 打印runApplication函数的定义
+// alert(AppRegistry.runApplication);
 AppRegistry.registerComponent('Box',() => appForweibo);
